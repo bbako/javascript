f005aad7219e30d7046e5141b44de1a88369c23d
young <young@DESKTOP-MR5V0T4>
2019-01-08 13:15:41 +0900

    db connection test

---
+++ test/src/test/java/org/phy/controller/TestJunit.java
@@ -0,0 +1,17 @@
+package org.phy.controller;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Handles requests for the application home page.
+ */
+public class TestJunit {
+	   @Test
+		
+	   public void testAdd() {
+	      String str = "Junit is working fine";
+	      assertEquals("Junit is working fine",str);
+	   }
+	}
